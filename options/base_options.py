ca8370f0050c5924d42e6327b67ac16f842198f0
itihandpose <96592818+itihandpose@users.noreply.github.com>
2022-01-08 23:05:32 +0200

    Add files via upload

--- options/base_options.py
+++ options/base_options.py
@@ -7,7 +7,8 @@ class BaseOptions():
 
     def initialize(self, parser):
         parser.add_argument('--local_rank', type=int, default=0)
-        parser.add_argument('--exp_name', type=str, default='test')
+        parser.add_argument('--exp_name', type=str, default='mobrecon')
+        # parser.add_argument('--exp_name', type=str, default='test')
         parser.add_argument('--n_threads', type=int, default=4)
         parser.add_argument('--device_idx', type=int, nargs='+', default=[-1,])
 
@@ -16,17 +17,22 @@ class BaseOptions():
         parser.add_argument('--pos_aug', type=float, default=3)
         parser.add_argument('--rot_aug', type=float, default=90)
         parser.add_argument('--color_aug', type=self.str2bool, default='yes')
-        parser.add_argument('--size', type=int, default=224)
+        parser.add_argument('--size', type=int, default=128)
+        # parser.add_argument('--size', type=int, default=224)
         parser.add_argument('--ms_mesh', type=self.str2bool, default='yes')
 
         # network hyperparameters
-        parser.add_argument('--out_channels', nargs='+', default=[64, 128, 256, 512], type=int)
+        parser.add_argument('--out_channels', nargs='+', default=[32, 64, 128, 256], type=int)
+        # parser.add_argument('--out_channels', nargs='+', default=[64, 128, 256, 512], type=int)
         parser.add_argument('--ds_factors', nargs='+', default=[2, 2, 2, 2], type=float)
         parser.add_argument('--in_channels', type=int, default=3)
-        parser.add_argument('--seq_length', type=int, default=[27, 27, 27, 27], nargs='+')
+        parser.add_argument('--seq_length', type=int, default=[9, 9, 9, 9], nargs='+')
+        # parser.add_argument('--seq_length', type=int, default=[27, 27, 27, 27], nargs='+')
         parser.add_argument('--dilation', type=int, default=[1, 1, 1, 1], nargs='+')
-        parser.add_argument('--model', type=str, default='cmr_sg')
-        parser.add_argument('--backbone', type=str, default='ResNet18')
+        parser.add_argument('--model', type=str, default='mobrecon')
+        # parser.add_argument('--model', type=str, default='cmr_sg')
+        parser.add_argument('--backbone', type=str, default='DenseStack')
+        # parser.add_argument('--backbone', type=str, default='ResNet18')
         parser.add_argument('--bn', type=self.str2bool, default='no')
         parser.add_argument('--att', type=self.str2bool, default='no')
 
@@ -39,10 +45,12 @@ class BaseOptions():
         parser.add_argument('--weight_decay', type=float, default=0)
 
         # training hyperparameters
-        parser.add_argument('--phase', type=str, default='train')
+        parser.add_argument('--phase', type=str, default='demo')
+        # parser.add_argument('--phase', type=str, default='train')
         parser.add_argument('--batch_size', type=int, default=32)
         parser.add_argument('--epochs', type=int, default=38)
-        parser.add_argument('--resume', type=str, default='')
+        parser.add_argument('--resume', type=str, default='mobrecon_densestack_dsconv.pt')
+        # parser.add_argument('--resume', type=str, default='')
 
         # others
         # parser.add_argument('--seed', type=int, default=1)
